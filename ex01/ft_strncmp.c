97b49d0aa36a6c0b559f7cf179a7df0859d55ba1
hshimizu <hisatosi35350@gmail.com>
2023-03-15 00:29:58 +0900

    前回のコミット、ex00の変更と同じ

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -6,24 +6,17 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 14:12:21 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/03/13 16:12:21 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/03/14 20:59:54 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	int	result;
-
-	result = 0;
-	while (*s1 && *s2 && n--)
+	while (*s1 && *s2 && n-- && *s1 == *s2)
 	{
-		if (*s1 != *s2)
-		{
-			result = 1 - 2 * (*s1 < *s2);
-			break ;
-		}
 		s1++;
 		s2++;
 	}
-	return (result);
+
+	return (*s1 - *s2);
 }
