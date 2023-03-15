6c9b2fd5786d47966cd819bac11657adb609c968
hshimizu <hisatosi35350@gmail.com>
2023-03-15 18:50:54 +0900

    除けるための条件が間違ってそうだった

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -6,13 +6,13 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 14:12:21 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/03/15 06:49:08 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/03/15 18:50:22 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	while (*s1 && *s2 && n-- && *s1 == *s2)
+	while (*s1 && *s2 && --n && *s1 == *s2)
 	{
 		s1++;
 		s2++;
