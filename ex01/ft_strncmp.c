001bb5eb63a098f704bafd3562aa86a0b8e6f92f
hshimizu <hisatosi35350@gmail.com>
2023-03-15 19:31:58 +0900

    おねっがいいいいいいいいいい泣き

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -6,16 +6,17 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 14:12:21 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/03/15 18:50:22 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/03/15 19:31:24 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	while (*s1 && *s2 && --n && *s1 == *s2)
+	while (*s1 && *s2 && 1 < n && *s1 == *s2)
 	{
 		s1++;
 		s2++;
+		n--;
 	}
 	return (*s1 - *s2);
 }
--- ex04/ft_strstr.c
+++ ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 17:07:59 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/03/15 19:00:03 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/03/15 19:31:41 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,24 +27,4 @@ char	*ft_strstr(char *str, char *to_find)
 		str++;
 	}
 	return ((void *)0);
-}
-
-#include        <stdio.h>
-#include        <strings.h>
-
-int main(void)
-{
-        char    s1[] = "abcdefghijklmn";        /* 検索対象文字列 */
-        char    s2[] = "defgh";                 /* 一致する場合 */
-        char    s3[] = "xyz";                   /* 一致しない場合 */
-        char    s4[] = "abcdefghijklmnopqr";    /* 検索対象文字列より長い場合 */
-        char    s5[] = "";                      /* \0 */
-        char    *sp;
-        
-        printf("一致する場合 : %s\n",ft_strstr(s1,s2));
-        printf("一致しない場合 : %s\n",ft_strstr(s1,s3));
-        printf("検索対象文字列より長い場合 : %s\n",ft_strstr(s1,s4));
-        printf("\\0 : %s\n",ft_strstr(s1,s5));
-        
-        return 0;
 }
