87a83eddf442f27639b67b389412c3e077d23736
hshimizu <hisatosi35350@gmail.com>
2023-03-15 19:27:16 +0900

    使わない関数のさくじょ

--- ex04/ft_strstr.c
+++ ex04/ft_strstr.c
@@ -6,19 +6,9 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 17:07:59 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/03/15 06:49:43 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/03/15 19:00:03 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-unsigned long	ft_strlen(char *str)
-{
-	unsigned long	i;
-
-	i = 0;
-	while (*(str + i))
-		i++;
-	return (i);
-}
 
 char	*ft_strstr(char *str, char *to_find)
 {
@@ -37,4 +27,24 @@ char	*ft_strstr(char *str, char *to_find)
 		str++;
 	}
 	return ((void *)0);
+}
+
+#include        <stdio.h>
+#include        <strings.h>
+
+int main(void)
+{
+        char    s1[] = "abcdefghijklmn";        /* 検索対象文字列 */
+        char    s2[] = "defgh";                 /* 一致する場合 */
+        char    s3[] = "xyz";                   /* 一致しない場合 */
+        char    s4[] = "abcdefghijklmnopqr";    /* 検索対象文字列より長い場合 */
+        char    s5[] = "";                      /* \0 */
+        char    *sp;
+        
+        printf("一致する場合 : %s\n",ft_strstr(s1,s2));
+        printf("一致しない場合 : %s\n",ft_strstr(s1,s3));
+        printf("検索対象文字列より長い場合 : %s\n",ft_strstr(s1,s4));
+        printf("\\0 : %s\n",ft_strstr(s1,s5));
+        
+        return 0;
 }
