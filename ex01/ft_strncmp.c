86a678be459fc866d7e4eabda62e593b52680f75
hshimizu <hisatosi35350@gmail.com>
2023-03-16 18:32:25 +0900

    全然違かったから修正

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -6,17 +6,26 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 14:12:21 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/03/16 14:12:15 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/03/16 18:31:57 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	while (*s1 && *s2 && 1 < n && *s1 == *s2)
+	while (n--)
 	{
-		s1++;
-		s2++;
-		n--;
+		if (*s1 != *s2++)
+			return (*(unsigned char *)s1 - *(unsigned char *)(s2 - 1));
+		if (!*s1++)
+			break ;
 	}
-	return ((unsigned char)*s1 - (unsigned char)*s2);
+	return (0);
+}
+
+#include <string.h>
+
+int	main(void)
+{
+	int i = ft_strncmp("aasd", "asdgfdf", 2);
+	int j = strncmp("aasd", "asdgfdf", 2);
 }
