82a34a540d852818f350324757eb3408fac41b3f
hshimizu <hisatosi35350@gmail.com>
2023-03-17 19:51:19 +0900

    あんさいんどに

--- ex05/ft_strlcat.c
+++ ex05/ft_strlcat.c
@@ -6,30 +6,30 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 13:46:23 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/03/16 20:38:32 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/03/17 19:50:34 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str)
+unsigned int	ft_strlen(char *str)
 {
-	int	i;
+	unsigned int	i;
 
 	i = 0;
-	while (*(str + i))
+	while (*(str++))
 		i++;
 	return (i);
 }
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	int	len_dest;
-	int	len_src;
+	unsigned int	len_dest;
+	unsigned int	len_src;
 
 	len_dest = ft_strlen(dest);
 	len_src = ft_strlen(src);
-	if ((int)size <= len_dest)
+	if (size <= len_dest)
 		return (size + len_src);
-	while (*src && (int)size-- > len_dest + 1)
+	while (*src && size-- > len_dest + 1)
 		*(dest++ + len_dest) = *src++;
 	*(dest + len_dest) = '\0';
 	return (len_dest + len_src);
